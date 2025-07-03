d1ce67812973c58d36db0ce523012d58d2910077
Filip746 <fkvesic@mathos.hr>
2025-07-03 11:58:27 +0200

    Match in-progress screen

--- app/game.tsx
+++ app/game.tsx
@@ -1,6 +1,7 @@
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { players } from '../components/PlayerSelection';
 import useCountdown from '../components/timer';
 
 export default function GameScreen() {
@@ -10,26 +11,74 @@ export default function GameScreen() {
   const minutes = Math.floor(timeLeft / 60000);
   const seconds = Math.floor((timeLeft % 60000) / 1000);
 
-  const { player1name, player2name } = useLocalSearchParams();
+  let imageSource = require('../assets/images/boy.png');
+  const { player1name, player2name, imagePath, player1id, player2id} = useLocalSearchParams();
+
+  const player1 = players.find(p => p.id === Number(player1id))?.avatar;
+  const player2 = players.find(p => p.id === Number(player2id))?.avatar;
+
+
 
   return (
-    <View style={styles.container}>
-        <Text style={styles.timer}>{player1name} vs {player2name}</Text>
-      <Text style={styles.timer}>
-        {`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}
-      </Text>
+  <View style={styles.container}>
+    <View style={styles.playersContainer}>
+      <View style={styles.player}>
+        <Text style={styles.title}>{player1name}</Text>
+        <Image source={player1} style={styles.avatar} />
+      </View>
+
+      <Text style={styles.vs}>vs</Text>
+
+      <View style={styles.player}>
+        <Text style={styles.title}>{player2name}</Text>
+        <Image source={player2} style={styles.avatar} />
+      </View>
     </View>
-  );
+
+    <Text style={styles.timer}>
+      {`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}
+    </Text>
+  </View>
+);
 }
 
 const styles = StyleSheet.create({
   container: { 
     flex: 1, 
     justifyContent: 'center', 
-    alignItems: 'center' 
-},
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+  playersContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
+  },
+  player: {
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  vs: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginHorizontal: 10,
+  },
   timer: { 
     fontSize: 48, 
-    fontWeight: 'bold' 
-},
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
--- components/PlayerSelection.tsx
+++ components/PlayerSelection.tsx
@@ -14,7 +14,7 @@ interface Player {
 
 
 
-const players: Player[] = [
+export const players: Player[] = [
   { id: 1, name: 'Marko', color: '#FFB6C1', image: require("../assets/images/croatia.png"), avatar: require("../assets/images/avatar.jpg")},
   { id: 2, name: 'Ivana', color: '#ADD8E6', image: require("../assets/images/germany.png"), avatar: require("../assets/images/girl.jpg")},
   { id: 3, name: 'Petar', color: '#90EE90', image: require("../assets/images/spain.png"), avatar: require("../assets/images/boy.png") },
@@ -87,6 +87,9 @@ export default function PlayerSelection() {
               params: {
                 player1name: players.find(p1 => p1.id === selectedPlayer1)?.name,
                 player2name: players.find(p2 => p2.id === selectedPlayer2)?.name,
+                imagePath: '../assets/images/croatia.png',
+                player1id: selectedPlayer1,
+                player2id: selectedPlayer2
               }
               })}>yes
             </Button>
