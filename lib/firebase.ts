53240ab7d75dafdf7b2bf60baa7d70cc4b3ae45f
Filip746 <fkvesic@mathos.hr>
2025-07-15 09:25:45 +0200

    Fixed bugs for .env with Expo

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,20 +3,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { API_KEY, APP_ID, AUTH_DOMAIN, MEASUREMENT_ID, MESSAGING_SENDER_ID, PROJECT_ID, STORAGE_BUCKET } from "@env";
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
--- types/env.d.ts
+++ types/env.d.ts
@@ -1,9 +1,9 @@
 declare module '@env' {
-  export const API_KEY: string;
-  export const AUTH_DOMAIN: string;
-    export const PROJECT_ID: string;
-    export const STORAGE_BUCKET: string;
-    export const MESSAGING_SENDER_ID: string;
-    export const APP_ID: string;
-    export const MEASUREMENT_ID: string;
+  export const EXPO_PUBLIC_API_KEY: string;
+  export const EXPO_PUBLIC_AUTH_DOMAIN: string;
+  export const EXPO_PUBLIC_PROJECT_ID: string;
+  export const EXPO_PUBLIC_STORAGE_BUCKET: string;
+  export const EXPO_PUBLIC_MESSAGING_SENDER_ID: string;
+  export const EXPO_PUBLIC_APP_ID: string;
+  export const EXPO_PUBLIC_MEASUREMENT_ID: string;
 }
