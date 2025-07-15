fd9ba7b96d8e8cb01cf4f0e0d23af1f9bf1e874e
Filip746 <fkvesic@mathos.hr>
2025-07-15 09:14:55 +0200

    Added .env dependency

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,17 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { API_KEY, APP_ID, AUTH_DOMAIN, MEASUREMENT_ID, MESSAGING_SENDER_ID, PROJECT_ID, STORAGE_BUCKET } from "@env";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdFYqANKMZ2d4aaGDwukEuDADdRny_EJg",
-  authDomain: "billiard-ee4dc.firebaseapp.com",
-  projectId: "billiard-ee4dc",
-  storageBucket: "billiard-ee4dc.firebasestorage.app",
-  messagingSenderId: "327785519622",
-  appId: "1:327785519622:web:f6a4038d21b53969d60187",
-  measurementId: "G-GL4GYV3FE5"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
