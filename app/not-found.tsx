f759feab4b34ce0adbc735d2bea0fa8b899293ad
Filip746 <fkvesic@mathos.hr>
2025-07-21 13:34:08 +0200

    Added Not Found route

---
+++ app/not-found.tsx
@@ -0,0 +1,29 @@
+import { useRouter } from 'expo-router';
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  const goHome = () => {
+    router.push('/')
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>404</Text>
+      <Text style={styles.subtitle}>Not found</Text>
+      <TouchableOpacity style={styles.button} onPress={goHome}>
+        <Text style={styles.buttonText}>Go back to Home</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff" },
+  title: { fontSize: 60, fontWeight: "bold", marginBottom: 10, color: "#be1e2d" },
+  subtitle: { fontSize: 20, color: "#333" },
+  button: { marginTop: 24, padding: 14, backgroundColor: "#be1e2d", borderRadius: 8 },
+  buttonText: { color: "#fff", fontWeight: "bold", fontSize: 16 }
+});
