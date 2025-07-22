910cb3a46e8831e268d865d03ec74d446398f00e
Filip746 <fkvesic@mathos.hr>
2025-07-22 15:45:01 +0200

    Added util Record to images path

--- const/images.ts
+++ const/images.ts
@@ -1,15 +1,15 @@
 import { ImageSourcePropType } from "react-native";
 
-export const billiard = require('@/assets/images/billiard.jpg');
-
-export const images: { [key: string]: ImageSourcePropType } = {
-  "croatia.png": require("../assets/images/croatia.png"),
-  "germany.png": require("../assets/images/germany.png"),
-  "spain.png": require("../assets/images/spain.png"),
+export const images: Record<string, ImageSourcePropType> = {
+  croatia: require("../assets/images/croatia.png"),
+  germany: require("../assets/images/germany.png"),
+  spain: require("../assets/images/spain.png"),
 };
 
-export const avatars: { [key: string]: ImageSourcePropType } = {
-  "avatar.jpg": require("../assets/images/avatar.jpg"),
-  "girl.jpg": require("../assets/images/girl.jpg"),
-  "boy.png": require("../assets/images/boy.png"),
+export const avatars: Record<string, ImageSourcePropType> = {
+  avatar: require("../assets/images/avatar.jpg"),
+  girl: require("../assets/images/girl.jpg"),
+  boy: require("../assets/images/boy.png"),
 };
+
+export const billiard: ImageSourcePropType = require("../assets/images/billiard.jpg");
